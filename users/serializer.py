8f6566f65eaa6e8bd6561664c59111519e355a2b
Guilherme Schulz <gui.schulz@gmail.com>
2023-02-13 22:35:08 -0300

    tarefa 1 finalizada

---
+++ users/serializer.py
@@ -0,0 +1,40 @@
+from rest_framework import serializers
+from .models import User
+from rest_framework.validators import UniqueTogetherValidator
+from rest_framework.serializers import ValidationError
+from rest_framework.validators import UniqueValidator
+
+
+class UserSerializer(serializers.Serializer):
+    id = serializers.IntegerField(read_only=True)
+    username = serializers.CharField(
+        required=True,
+        validators=[
+            UniqueValidator(
+                queryset=User.objects.all(), message="username already taken."
+            )
+        ],
+    )
+    email = serializers.CharField(
+        max_length=127,
+        required=True,
+        validators=[
+            UniqueValidator(
+                queryset=User.objects.all(), message="email already registered."
+            )
+        ],
+    )
+    password = serializers.CharField(write_only=True)
+    birthdate = serializers.DateField(required=False)
+    first_name = serializers.CharField(max_length=50)
+    last_name = serializers.CharField(max_length=50)
+    is_employee = serializers.BooleanField(allow_null=True, default=False)
+    is_superuser = serializers.BooleanField(read_only=True)
+
+    def validate_is_employee(self, value: bool):
+        return value
+
+    def create(self, validated_data: dict):
+        if validated_data["is_employee"] is True:
+            return User.objects.create_superuser(**validated_data, is_superuser=True)
+        return User.objects.create_user(**validated_data)
